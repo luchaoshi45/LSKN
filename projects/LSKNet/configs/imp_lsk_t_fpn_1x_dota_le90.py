f7528f922b58a44758f604add7686c9c7c5fa8a0
luchaoshi45 <luchaoshi45@gmail.com>
2023-05-05 16:52:46 +0800

    first commit

---
+++ projects/LSKNet/configs/imp_lsk_t_fpn_1x_dota_le90.py
@@ -0,0 +1,31 @@
+_base_ = [
+    './lsk_t_fpn_1x_dota_le90.py',
+]
+angle_version = 'le90'
+
+train_pipeline = [
+    dict(type='mmdet.LoadImageFromFile', backend_args=None),
+    dict(type='mmdet.LoadAnnotations', with_bbox=True, box_type='qbox'),
+    dict(type='ConvertBoxType', box_type_mapping=dict(gt_bboxes='rbox')),
+    dict(type='mmdet.Resize', scale=(400, 400), keep_ratio=True),
+    dict(
+        type='mmdet.RandomFlip',
+        prob=0.75,
+        direction=['horizontal', 'vertical', 'diagonal']),
+    dict(
+        type='RandomRotate',
+        prob=0.5,
+        angle_range=180,
+        rect_obj_labels=[9, 11]),
+    dict(type='mmdet.PackDetInputs')
+]
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    dataset=dict(
+        pipeline=train_pipeline))
+
+model = dict(
+    backbone=dict(
+        norm_cfg=dict(type='BN', requires_grad=True)),
+)
